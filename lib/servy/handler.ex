2c3596cbca79dae7ba917efaef45bf9725f794d2
Ismael G Marin C <ismael.marin@gmail.com>
2026-07-09 21:39:47 -0600

    Improved method

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -81,19 +81,18 @@ defmodule Servy.Handler do
     [request_line | header_lines] = String.split(top, "\n")
     [method, path, _] = String.split(request_line, " ")
 
-    headers = parse_header(header_lines, %{})
+    headers = parse_headers(header_lines)
     params = parse_params(headers["Content-Type"], params_string)
 
     %Conv{method: method, path: path, params: params, headers: headers}
   end
 
-  def parse_header([head | tail], headers) do
-    [key, value] = String.split(head, ": ")
-    headers = Map.put(headers, key, value)
-    parse_header(tail, headers)
+  def parse_headers(header_lines) do
+    Enum.reduce(header_lines, %{}, fn line, headers_so_far ->
+      [key, value] = String.split(line, ": ")
+      Map.put(headers_so_far, key, value)
+    end)
   end
-
-  def parse_header([], headers), do: headers
 
   def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim() |> URI.decode_query()
