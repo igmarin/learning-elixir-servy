4ea15a9aa6b6ce5f1b6b5c2b7c94d7763bae62f8
Ismael G Marin C <ismael.marin@gmail.com>
2026-07-20 13:53:33 -0600

    Concurrency part 1

--- lib/servy/http_server.ex
+++ lib/servy/http_server.ex
@@ -47,8 +47,9 @@ defmodule Servy.HttpServer do
 
     IO.puts("⚡️  Connection accepted!\n")
 
-    # Receives the request and sends a response over the client socket.
-    serve(client_socket)
+    # Receives the request and sends a response over the client socket after
+    # spawning a new process to avoid blocking the accept loop.
+    spawn(fn -> serve(client_socket) end)
 
     # Loop back to wait and accept the next connection.
     accept_loop(listen_socket)
